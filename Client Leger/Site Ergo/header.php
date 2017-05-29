a7bf62e37458146b018ffbca380d1165fbf3d394
samy tebbal <samy tebbal>
2017-05-29 07:59:58 +0200

    MAJ Wording

---
+++ Client Leger/Site Ergo/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+  <div style="height:20%;text-align:center;background-color:#000000"><img src="img/BAN.bmp"></div>
+    <table id="en-tete">
+      <tbody>
+        <tr>
+          <td><a href="accueil.html"><img style="width: 108px; height: 55px;" src="img/accueil.gif"> </a><br>
+          </td>
+          <td> <a href="ergo.html">L'ergothérapie</a> </td>
+          <td> <a href="consultation.html">Motif de consultation</a> </td>
+          <td> <a href="financement.html">Financement</a> </td>
+          <td> <a href="tarif.html">Tarifs</a> </td>
+		  <td> <a href="contact.html">Contact</a> </td>
+          <td style="background-image: url();"><br>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
